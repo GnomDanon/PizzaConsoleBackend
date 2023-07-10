da3213d3c7e3fbf2f62b8295f0079c17e3f8b83d
Elm-TK <Temirlan.Elmuratov@urfu.me>
2023-07-10 12:25:10 +0400

    add ErrorHandlingMiddleware

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router = new Router()
+//const Router = require('./Router')
+//const Router = require('./Router')
+//const Router = require('./Router')
+//const Router = require('./Router')
+
+//router.use('/', Router)
+//router.use('/', Router)
+//router.use('/', Router)
+//router.use('/', Router)
+
+module.exports = router
