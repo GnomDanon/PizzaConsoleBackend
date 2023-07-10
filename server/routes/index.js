1f9a1cee4dcda315e93bac2785fc1499d0b55a46
Elm-TK <Temirlan.Elmuratov@urfu.me>
2023-07-10 12:50:47 +0400

    add userController add userRoutes add authMiddleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const Router = require('express')
 const router = new Router()
-//const Router = require('./Router')
+const userRouter = require('./userRouter')
 //const Router = require('./Router')
 //const Router = require('./Router')
 //const Router = require('./Router')
 
-//router.use('/', Router)
+router.use('/user', userRouter)
 //router.use('/', Router)
 //router.use('/', Router)
 //router.use('/', Router)
