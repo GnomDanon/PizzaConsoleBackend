79d0a48d6fdb998225d1bbbfb4cdfea9f926b48c
GaripovDaniil <Garipov.Daniil@urfu.me>
2023-07-13 10:40:46 +0500

    Добавление ингредиентов

--- server/controllers/ingredientsController.js
+++ server/controllers/ingredientsController.js
@@ -2,6 +2,16 @@ const ApiError = require('../error/ApiError')
 const {Ingredients} = require('../models/models')
 
 class IngredientsController {
+    async createIngredient(req, res, next) {
+        try {
+            const {title, count} = req.body
+            const ingredient = Ingredients.create({title, count})
+            return res.json(ingredient)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+    }
+
     async getIngredients(req, res, next) {
         try {
             const ingredients = await Ingredients.findAll()
--- server/routes/ingredientsRouter.js
+++ server/routes/ingredientsRouter.js
@@ -2,6 +2,8 @@ const Router = require('express')
 const router = new Router()
 const ingredientsController = require('../controllers/ingredientsController')
 
+router.post('/createIngredient', ingredientsController.createIngredient)
+
 router.get('/getIngredients', ingredientsController.getIngredients)
 
 router.put('/changeIngredientCountByID', ingredientsController.changeById)
