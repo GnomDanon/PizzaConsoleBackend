f2348518afe7b239f109c6170dbf36c57b0804f1
GaripovDaniil <Garipov.Daniil@urfu.me>
2023-07-13 23:25:01 +0500

    Исправление ошибок

--- server/routes/ordersRouter.js
+++ server/routes/ordersRouter.js
@@ -4,11 +4,7 @@ const ordersController = require('../controllers/ordersController')
 
 router.get('/getAllOrders', ordersController.getAllOrders)
 router.post('/getOneOrderByID', ordersController.getOneOrderByID)
-<<<<<<< HEAD
 
 router.put('/changeStatusByID', ordersController.changeStatusByID)
-=======
-router.put('/changeStatucByID', ordersController.changeStatusByID)
->>>>>>> 9b8d8cdc9c2aee5c022ccf072e693f9d24fefba0
 
 module.exports = router
